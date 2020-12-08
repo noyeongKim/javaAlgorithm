55df8605968f5fe760f7533283a282613a4071e4
noyeongKim <padung123@naver.com>
2020-12-08 19:08:54 +0900

    [2020-12-08] > 시험성적, 윤년 테스트 진행

--- src/main/java/ifTest/Main.java
+++ src/main/java/ifTest/Main.java
@@ -14,13 +14,54 @@ public class Main {
          */
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
-        int b = sc.nextInt();
+        /*int b = sc.nextInt();
         if(a > b) {
             System.out.println(">");
         } else if (a < b) {
             System.out.println("<");
         } else {
             System.out.println("==");
+        }*/
+
+        ////////////////////////////////////////////////////////////////////////////////////////////
+        // 2020.12.08
+
+        /*
+        문제번호 9498. 성적 표시
+        시험 점수를 입력받아
+        90 ~ 100점은 A,
+        80 ~ 89점은 B,
+        70 ~ 79점은 C,
+        60 ~ 69점은 D,
+        나머지 점수는 F를 출력하는 프로그램을 작성하시오.
+         */
+
+        /*if(a >= 90 && a <= 100){
+            System.out.println("A");
+        } else if (a >= 80 && a <= 89) {
+            System.out.println("B");
+        } else if (a >= 70 && a < 80) {
+            System.out.println("C");
+        } else if (a >= 60 && a < 70) {
+            System.out.println("D");
+        } else {
+            System.out.println("F");
+        }*/
+
+        /*
+        문제번호 2753. 윤년표시
+        연도가 주어졌을 때, 윤년이면 1, 아니면 0을 출력하는 프로그램을 작성하시오.
+        윤년은 연도가 4의 배수이면서, 100의 배수가 아닐 때 또는 400의 배수일 때이다.
+        예를 들어, 2012년은 4의 배수이면서 100의 배수가 아니라서 윤년이다.
+        1900년은 100의 배수이고 400의 배수는 아니기 때문에 윤년이 아니다.
+        하지만, 2000년은 400의 배수이기 때문에 윤년이다.
+         */
+
+        if((a % 4 == 0) && (a % 100 != 0 || a % 400 == 0)){
+            System.out.println("1");
+        } else {
+            System.out.println("0");
         }
+
     }
 }
