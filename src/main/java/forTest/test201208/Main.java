a9ead524742bb66c825f039ef4f28136cc720464
noyeongKim <househusband813@gmail.com>
2020-12-14 18:37:34 +0900

    [2020-12-14] > for 문 테스트 진행중 > 덧셈 프로그래밍 완료

--- src/main/java/forTest/test201208/Main.java
+++ src/main/java/forTest/test201208/Main.java
@@ -1,19 +1,56 @@
 package forTest.test201208;
 
+import java.io.BufferedReader;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
         /*
         문제번호 2739. 구구단단
         */
 
+        /*
+        int a = sc.nextInt();
         for(int i = 0; i < 9; i++){
             i++;
             System.out.println(a + " * " + i + " = " + (a * i ));
             i--;
+        }*/
+
+        /*
+        문제번호 10950. 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성
+        첫째 줄에 테스트 케이스의 개수 T가 주어진다.
+        각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. (0 < A, B < 10)
+         */
+
+        /*int t = sc.nextInt();
+        int a;
+        int b;
+
+        for(int i = 0; i < t; i++){
+            a = sc.nextInt();
+            b = sc.nextInt();
+            System.out.println(a+b);
+        }*/
+
+        /*
+        문제번호 8393. n이 주어졌을 때, 1부터 n까지 합을 구하는 프로그램을 작성하시오.
+        1부터 n까지 합을 출력한다.
+         */
+
+        /*int a = sc.nextInt();
+        int res = 0;
+
+        for(int i = 0; i < a; i++){
+            res = res + i + 1;
         }
+
+        System.out.println(res);*/
+
+        /*
+        문제번호 15552.
+         */
+
     }
 }
